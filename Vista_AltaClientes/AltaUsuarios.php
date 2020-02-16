9b3f98af809cfd32e59db94e6925b9fa410bcc64
ovaldez <omar.valdez.becerril@gmail.com>
2020-02-15 19:41:34 -0600

    Inicio de aplicación, mini sap

---
+++ Vista_AltaClientes/AltaUsuarios.php
@@ -0,0 +1,12 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Codeigniter MongoDB Create Read Update Delete Example</title>
+    </head>
+    <body>
+	</body>
+<html>
