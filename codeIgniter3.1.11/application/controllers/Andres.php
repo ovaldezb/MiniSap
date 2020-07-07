1df5eb036748c75dd612ed3ef760f3157d279951
Andres Coraza <andres_ezio@hotmail.com>
2020-07-07 12:58:34 -0500

    prueba repo andres

---
+++ codeIgniter3.1.11/application/controllers/Andres.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Andres extends CI_Controller
+{
+	function __construct() {
+		parent::__construct();
+	}
+
+    function index() 
+    {
+
+	}
+
+
+}
+
+?>
